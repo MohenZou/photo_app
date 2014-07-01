3e097c9324ee7f74cf4bb342de5cd2115924b854
Muhan Zou <muhan.zou@oracle.com>
2014-06-30 17:28:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameLearningApp::Application.config.secret_key_base = '910b6ac5043aea83a9499c5c04563b69863e60eff4be89322c5c0de84b98d8648783eb0c2cca827ba575f388f33cf60f64e6be8624511bf876d99fd393755a81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GameLearningApp::Application.config.session_store :cookie_store, key: '_game_learning_app_session'
